539a02990df809e69d5436d74bad4860212e062c
falbuisech <f.galbuisech@gmail.com>
2015-05-16 04:44:21 +0200

    Editar preguntas

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -67,3 +67,24 @@ exports.new = function(req, res){
     }
    });
  }
+
+//GET /quizes/:id/edit
+exports.edit= function(req, res){
+  var quiz= req.quiz;
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+}
+
+//PUT /quizes/:id
+exports.update= function(req, res){
+  req.quiz.pregunta=req.body.quiz.pregunta;
+  req.quiz.respuesta=req.body.quiz.respuesta;
+
+  req.quiz.validate().then(function(err){
+    if(err){
+      res.render('quizes/new', {quiz: req.quiz, errors: err.errors});
+    }else{
+      req.quiz.save({fields: ["pregunta", "respuesta"]})
+      .then(function(){res.redirect('/quizes');});
+    }
+  })
+}
