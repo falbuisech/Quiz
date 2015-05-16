eb12c7112236d175fbdd8abef19587902af8cfad
falbuisech <f.galbuisech@gmail.com>
2015-05-16 04:55:44 +0200

    Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
 
-//Autoload
+/*Autoload*/
 router.param('quizId', quizController.load);
 
 
@@ -18,14 +18,16 @@ router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 router.get('/quizes/new',                   quizController.new);
 router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
+router.get('/quizes/answer',		   		quizController.answer);
 router.put('/quizes/:quizId(\\d+)', 		quizController.update);
 router.post('/quizes/create',               quizController.create);
+router.delete('/quizes/:quizId(\\d+)', 		quizController.destroy);
 
 router.get('/author', function(req, res){
 	res.render("author", {errors: []})});
 
 
-/* GET pagina respuestas */
-router.get('/quizes/answer',		    quizController.answer);
+
+
 
 module.exports = router;
