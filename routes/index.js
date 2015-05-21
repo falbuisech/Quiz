05839b86fd442065b374723f8e656696913f126a
falbuisech <f.galbuisech@gmail.com>
2015-05-21 21:50:41 +0200

    Mis preguntas

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,7 @@ router.post('/user', userController.create);
 router.get('/user/:userId(\\d+)/edit', 	sessionController.loginRequired, userController.ownershipRequired, userController.edit);
 router.put('/user/:userId(\\d+)', 		sessionController.loginRequired, userController.ownershipRequired, userController.update);
 router.delete('/user/:userId(\\d+)', 	sessionController.loginRequired, userController.ownershipRequired, userController.destroy);
+router.get('/user/:userId(\\d+)/quizes', quizController.index);
 
 /* rutas de quizes */
 router.get('/quizes',                       quizController.index);
